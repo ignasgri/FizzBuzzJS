9c26991cf920678e469e4d4bba2a640228c9e9bd
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-03 09:07:41 +0000

    remainder of 3 and 5 FizzBuzz

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 for (var i = 1; i <= 100; i++){
+    //remainder if 3 and 5
+    if (i % 3 == 0 && i % 5 == 0)
+        console.log('FizzBuzz');
     //remainder of 3
-    if (i % 3 == 0)
+    else if (i % 3 == 0)
         console.log('Fizz');
     //remainder of 5
     else if (i % 5 == 0)
