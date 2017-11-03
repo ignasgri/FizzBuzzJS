451c8ae4ed694b373685ad0fd77f4340224317c1
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-03 10:41:53 +0000

    Fizz and Buzz number values working

--- sketch.js
+++ sketch.js
@@ -1,33 +1,29 @@
 function setup(){
-    // var Fizz = document.getElementById('#fizz');
-    // var Buzz = document.getElementById('#buzz');
-    // var num = document.getElementById('#num');
     var button = select('#submit');
     button.mousePressed(submitValue);
 }
 
 function submitValue () {
-    // var Fizz = select('#fizz').value();
-    // var Buzz = select('#buzz').value();
     var num = select('#num').value();
-    FizzBuzz(num);
+    FizzBuzz(num, fizz, buzz);
 }
-var num = document.getElementById('#num');
-function FizzBuzz(number){
-for (var i = 1; i <= number; i++){
-    //remainder if 3 and 5
-    if (i % 3 == 0 && i % 5 == 0){
-        console.log('FizzBuzz');
-    //remainder of 3
-    } else if (i % 3 == 0){
-        console.log('Fizz');
-    //remainder of 5
-    } else if (i % 5 == 0){
-        console.log('Buzz');
-    } else {
-    //print number
-        console.log(i);
+
+function FizzBuzz(num){
+    var Fizz = select('#fizz').value();
+    var Buzz = select('#buzz').value();
+    for (var i = 1; i <= num; i++){
+        //remainder if 3 and 5
+        if (i % Fizz == 0 && i % Buzz == 0){
+            console.log('FizzBuzz');
+        //remainder of 3
+        } else if (i % Fizz == 0){
+            console.log('Fizz');
+        //remainder of 5
+        } else if (i % Buzz == 0){
+            console.log('Buzz');
+        } else {
+        //print number
+            console.log(i);
+        }
     }
 }
-}
-FizzBuzz(num);
