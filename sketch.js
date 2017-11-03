312e127526f0da89a97e5f55f756ff132f9d7f11
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-03 09:05:25 +0000

    remainder of 5 Buzz

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ for (var i = 1; i <= 100; i++){
     //remainder of 3
     if (i % 3 == 0)
         console.log('Fizz');
+    //remainder of 5
+    else if (i % 5 == 0)
+        console.log('Buzz');
     else
     //print number
         console.log(i);
