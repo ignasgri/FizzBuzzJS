b00df0e083fe53a03f1ab9869a9d6d35b63c2ce7
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-03 10:09:22 +0000

    UI input fields added, number is working

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
-for (var i = 1; i <= 100; i++){
+function setup(){
+    // var Fizz = document.getElementById('#fizz');
+    // var Buzz = document.getElementById('#buzz');
+    // var num = document.getElementById('#num');
+    var button = select('#submit');
+    button.mousePressed(submitValue);
+}
+
+function submitValue () {
+    // var Fizz = select('#fizz').value();
+    // var Buzz = select('#buzz').value();
+    var num = select('#num').value();
+    FizzBuzz(num);
+}
+var num = document.getElementById('#num');
+function FizzBuzz(number){
+for (var i = 1; i <= number; i++){
     //remainder if 3 and 5
-    if (i % 3 == 0 && i % 5 == 0)
+    if (i % 3 == 0 && i % 5 == 0){
         console.log('FizzBuzz');
     //remainder of 3
-    else if (i % 3 == 0)
+    } else if (i % 3 == 0){
         console.log('Fizz');
     //remainder of 5
-    else if (i % 5 == 0)
+    } else if (i % 5 == 0){
         console.log('Buzz');
-    else
+    } else {
     //print number
         console.log(i);
+    }
 }
+}
+FizzBuzz(num);
