482c8fd38cd9bbefad937174495e8db461fcea81
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-04 22:13:03 +0000

    bootstarp added

--- sketch.js
+++ sketch.js
@@ -15,15 +15,23 @@ function FizzBuzz(num){
         //remainder if 3 and 5
         if (i % Fizz == 0 && i % Buzz == 0){
             console.log('FizzBuzz');
+            // document.write('FizzBuzz');
+            // document.write('<br/>');
         //remainder of 3
         } else if (i % Fizz == 0){
             console.log('Fizz');
+            // document.write('Fizz');
+            // document.write('<br/>');
         //remainder of 5
         } else if (i % Buzz == 0){
             console.log('Buzz');
+            // document.write('Buzz');
+            // document.write('<br/>');
         } else {
         //print number
             console.log(i);
+            // document.write(i);
+            // document.write('<br/>');
         }
     }
 }
