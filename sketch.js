74a22da31a02abd08cb536b7832c4c951973a8e7
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-05 22:23:26 +0000

    results added to index.html, fizz buzz value updating in index.html

--- sketch.js
+++ sketch.js
@@ -5,33 +5,37 @@ function setup(){
 
 function submitValue () {
     var num = select('#num').value();
+    document.getElementById("num_html").innerHTML = num;
     FizzBuzz(num, fizz, buzz);
 }
 
 function FizzBuzz(num){
     var Fizz = select('#fizz').value();
     var Buzz = select('#buzz').value();
+    var elem = document.getElementById('output');
+    //export to body element
+    document.getElementById("fizz_html").innerHTML = Fizz;
+    document.getElementById("fizz_var").innerHTML = Fizz;
+    document.getElementById("buzz_html").innerHTML = Buzz;
+    document.getElementById("buzz_var").innerHTML = Buzz;
     for (var i = 1; i <= num; i++){
         //remainder if 3 and 5
         if (i % Fizz == 0 && i % Buzz == 0){
             console.log('FizzBuzz');
-            // document.write('FizzBuzz');
-            // document.write('<br/>');
+            elem.insertAdjacentHTML('beforeend','FizzBuzz' + '</br>');
         //remainder of 3
         } else if (i % Fizz == 0){
             console.log('Fizz');
-            // document.write('Fizz');
-            // document.write('<br/>');
+            elem.insertAdjacentHTML('beforeend','Fizz' + '</br>') ;
         //remainder of 5
         } else if (i % Buzz == 0){
             console.log('Buzz');
-            // document.write('Buzz');
-            // document.write('<br/>');
+            elem.insertAdjacentHTML('beforeend','Buzz' + '</br>');
         } else {
         //print number
             console.log(i);
-            // document.write(i);
-            // document.write('<br/>');
+            elem.insertAdjacentHTML('beforeend',i + '</br>');
+
         }
     }
 }
